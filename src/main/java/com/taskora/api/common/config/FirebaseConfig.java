51602e262f22764a55dc5957acc3bff5de3c56b0
Emac56 <emacvicente@gmail.com>
2026-08-14 15:24:44 +0800

    feat: add Firebase Admin SDK initialization config

--- src/main/java/com/taskora/api/common/config/FirebaseConfig.java
+++ src/main/java/com/taskora/api/common/config/FirebaseConfig.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
