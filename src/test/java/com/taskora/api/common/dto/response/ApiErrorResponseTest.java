574f3109094afffeef984fdc80c4648555930f9c
Emac Vicente <emacvicente8@gmail.com>
2026-08-21 12:48:10 +0800

    Update and rename ApiErrorResponse.java to ApiErrorResponseTest.java

--- src/test/java/com/taskora/api/common/dto/response/ApiErrorResponseTest.java
+++ src/test/java/com/taskora/api/common/dto/response/ApiErrorResponseTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
-class ApiErrorResponseTest {
+public class ApiErrorResponseTest {
 
     @Test
     void shouldSetAndGetApiErrorResponseValues() {
