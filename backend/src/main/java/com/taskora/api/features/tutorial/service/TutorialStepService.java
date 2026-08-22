b78aae331e8f54b2451e67755f4ba75481a31c1c
Emac Vicente <emacvicente8@gmail.com>
2026-08-23 05:00:51 +0800

    Rename ReplaceTutorialStepsRequest to ReplaceTutorialStepRequest

--- backend/src/main/java/com/taskora/api/features/tutorial/service/TutorialStepService.java
+++ backend/src/main/java/com/taskora/api/features/tutorial/service/TutorialStepService.java
@@ -3,7 +3,7 @@ package com.taskora.api.features.tutorial.service;
 import java.util.List;
 
 import com.taskora.api.features.tutorial.dto.request.CreateTutorialStepRequest;
-import com.taskora.api.features.tutorial.dto.request.ReplaceTutorialStepsRequest;
+import com.taskora.api.features.tutorial.dto.request.ReplaceTutorialStepRequest;
 import com.taskora.api.features.tutorial.dto.request.UpdateTutorialStepRequest;
 import com.taskora.api.features.tutorial.dto.response.TutorialStepResponse;
 
