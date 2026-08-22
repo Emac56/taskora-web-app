c43fff9adf4c5c3c21c61ac5c566395145b21b51
Emac Vicente <emacvicente8@gmail.com>
2026-08-22 09:19:09 +0800

    Add countByStatus method to TutorialRepository

--- backend/src/main/java/com/taskora/api/features/tutorial/repository/TutorialRepository.java
+++ backend/src/main/java/com/taskora/api/features/tutorial/repository/TutorialRepository.java
@@ -3,6 +3,9 @@ package com.taskora.api.features.tutorial.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.taskora.api.features.tutorial.entity.Tutorial;
+import com.taskora.api.features.tutorial.enums.TutorialStatus;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+
+    long countByStatus(TutorialStatus status);
 }
