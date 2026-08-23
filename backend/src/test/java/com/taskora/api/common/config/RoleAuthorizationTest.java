386488edce6f34e444f3dff0ff10bec29ec3cb88
Emac Vicente <emacvicente8@gmail.com>
2026-08-23 13:31:59 +0800

    Add import for TutorialStatsResponse in RoleAuthorizationTest

--- backend/src/test/java/com/taskora/api/common/config/RoleAuthorizationTest.java
+++ backend/src/test/java/com/taskora/api/common/config/RoleAuthorizationTest.java
@@ -1,5 +1,6 @@
 package com.taskora.api.common.config;
 
+import com.taskora.api.features.tutorial.dto.response.TutorialStatsResponse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
